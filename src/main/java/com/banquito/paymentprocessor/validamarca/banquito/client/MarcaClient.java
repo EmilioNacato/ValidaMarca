2071968d65f0c93a28e91a2364613507fa31875b
Anthony Quishpe <aequishpe2@espe.edu.ec>
2025-02-24 12:50:11 -0500

    Refactorizar Valida Marca

--- src/main/java/com/banquito/paymentprocessor/validamarca/banquito/client/MarcaClient.java
+++ src/main/java/com/banquito/paymentprocessor/validamarca/banquito/client/MarcaClient.java
@@ -4,12 +4,13 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
-import com.banquito.paymentprocessor.validamarca.banquito.client.dto.MarcaRequest;
+//import com.banquito.paymentprocessor.validamarca.banquito.client.dto.MarcaRequest;
 import com.banquito.paymentprocessor.validamarca.banquito.client.dto.MarcaResponse;
+import com.banquito.paymentprocessor.validamarca.banquito.controller.dto.ValidacionMarcaRequestDTO;
 
 @FeignClient(name = "marca-externa", url = "${app.marca-externa.url}")
 public interface MarcaClient {
     
     @PostMapping("/api/v1/validate")
-    MarcaResponse validarTarjeta(@RequestBody MarcaRequest request);
+    MarcaResponse validarTarjeta(@RequestBody ValidacionMarcaRequestDTO request);
 } 
